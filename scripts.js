5cdd4c1cf58646ad6004eaa6d5b888a7b90bd1fe
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-05-23 16:49:45 -0400

    Added start button which lets user determine size of board

--- scripts.js
+++ scripts.js
@@ -1,28 +1,40 @@
-// creates 16 rows with unique ids
-for(let i=0; i<16; i++){
-    let row = document.createElement("row");
-    row.classList.add('row');
-    row.setAttribute("id", `row${i+1}`);
-    container.appendChild(row);
-}
+let startButton = document.getElementById("button");
+startButton.addEventListener("click", () => {
+    let userInput = prompt("How many squares per side would you like your board to be between 2 and 100?");
+    let boardSize = Number(userInput);
 
-//creates 16 square divs per row
-for(let i=0; i<16; i++){
-    let row = document.getElementById(`row${i+1}`);
-    for(let j=0; j<16; j++){
-        let col = document.createElement("div");
-        col.classList.add("col");
-        row.appendChild(col);
+    // creates rows according to user input with unique ids
+    for(let i=0; i<boardSize; i++){
+        let row = document.createElement("row");
+        row.classList.add('row');
+        row.setAttribute("id", `row${i+1}`);
+        let heightInPercent = 100/boardSize;
+        row.style.height = `${heightInPercent}%`;
+        container.appendChild(row);
+    }
+
+    //creates squares dynamically according to user input for each row
+    for(let i=0; i<boardSize; i++){
+        let row = document.getElementById(`row${i+1}`);
+        for(let j=0; j<boardSize; j++){
+            let col = document.createElement("div");
+            let widthInPercent = 100/boardSize;
+            col.style.width = `${widthInPercent}%`;
+            col.classList.add("col");
+            row.appendChild(col);
+        };
     };
-};
 
-let nodeListOfTiles = document.getElementsByClassName("col");
-for(let i=0; i<256; i++){
-    let tile = nodeListOfTiles[i];
-    tile.addEventListener("mouseover", () => {
-        tile.classList.add("hover");
-    });
-};
+    //adds hover effect to all squares in grid
+    let nodeListOfTiles = document.getElementsByClassName("col");
+    for(let i=0; i<boardSize**2; i++){
+        let tile = nodeListOfTiles[i];
+        tile.addEventListener("mouseover", () => {
+            tile.classList.add("hover");
+        });
+    };
+
+});
 
 
 
