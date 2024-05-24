6de5a91e2752e89ab6678bfda16881f4d2567f31
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-05-24 15:29:25 -0400

    Set upper limit for user input

--- scripts.js
+++ scripts.js
@@ -1,27 +1,32 @@
 let startButton = document.getElementById("button");
-startButton.addEventListener("click", () => {    
-    let userInput = prompt("How many squares per side would you like your board to be between 2 and 100?");
-    let boardSize = Number(userInput);
 
-    //remove old board
+startButton.addEventListener("click", () => {
     container.replaceChildren();
+    let gridSize = prompt("How many squares per side would you like your board to be between 2 and 100?");
+    if (gridSize > 100){
+        alert("Upper limit reached. Try again, or start with this 16x16 sketch pad!")
+        gridSize = 16;
+    }
+    createSketchBoard(gridSize);
+});
 
+function createSketchBoard (gridSize) {
     // creates rows according to user input with unique ids
-    for(let i=0; i<boardSize; i++){
+    for(let i=0; i<gridSize; i++){
         let row = document.createElement("row");
         row.classList.add('row');
         row.setAttribute("id", `row${i+1}`);
-        let heightInPercent = 100/boardSize;
+        let heightInPercent = 100/gridSize;
         row.style.height = `${heightInPercent}%`;
         container.appendChild(row);
     }
 
     //creates squares dynamically according to user input for each row
-    for(let i=0; i<boardSize; i++){
+    for(let i=0; i<gridSize; i++){
         let row = document.getElementById(`row${i+1}`);
-        for(let j=0; j<boardSize; j++){
+        for(let j=0; j<gridSize; j++){
             let col = document.createElement("div");
-            let widthInPercent = 100/boardSize;
+            let widthInPercent = 100/gridSize;
             col.style.width = `${widthInPercent}%`;
             col.classList.add("col");
             col.setAttribute("id", `col${j+1}`);
@@ -31,10 +36,11 @@ startButton.addEventListener("click", () => {
 
     //adds hover effect to all squares in grid
     let nodeListOfTiles = document.getElementsByClassName("col");
-    for(let i=0; i<boardSize**2; i++){
+    for(let i=0; i<gridSize**2; i++){
         let tile = nodeListOfTiles[i];
         tile.addEventListener("mouseover", () => {
             tile.classList.add("hover");
         });
     };
-});
+};
+        
